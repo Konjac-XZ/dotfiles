492b6a1277395f511fdfaff3717966ef776e3e09
Konjac-XZ <1951801592@qq.com>
2026-02-09 14:42:26 +0800

    Update .zsh/40-aliases.zsh

--- dot_zsh/40-aliases.zsh
+++ dot_zsh/40-aliases.zsh
@@ -1,8 +1,6 @@
 # Aliases
 
 alias rp='realpath'
-alias claude="claude --dangerously-skip-permissions"
-alias codex="codex --dangerously-bypass-approvals-and-sandbox"
 alias cmt="claude --model haiku \"commit with your skill\""
 alias fd="fd -HI -c always"
 alias ll="eza -la --group-directories-first --icons --git --color=always"
