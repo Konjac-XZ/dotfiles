125e3963b44db2a9ec905290db8fa7814449741b
Konjac-XZ <1951801592@qq.com>
2026-02-15 17:27:40 +0800

    Update .zsh/50-functions.zsh

--- dot_zsh/50-functions.zsh
+++ dot_zsh/50-functions.zsh
@@ -107,3 +107,11 @@ function cf() {
     [[ -n "$*" ]] && echo "No matches found for: $*"
   fi
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
