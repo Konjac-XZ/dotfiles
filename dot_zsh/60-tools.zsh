9f4fe4d19588d08198d985ab068c41bd61c11029
Konjac-XZ <1951801592@qq.com>
2026-02-26 11:42:04 +0800

    Update .zsh/60-tools.zsh

--- dot_zsh/60-tools.zsh
+++ dot_zsh/60-tools.zsh
@@ -2,37 +2,8 @@
 
 # NVM (Node Version Manager)
 export NVM_DIR="$HOME/.nvm"
-
-function __load_nvm() {
-  unset -f nvm node npm npx corepack __load_nvm
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-}
-
-function nvm() {
-  __load_nvm
-  nvm "$@"
-}
-
-function node() {
-  __load_nvm
-  node "$@"
-}
-
-function npm() {
-  __load_nvm
-  npm "$@"
-}
-
-function npx() {
-  __load_nvm
-  npx "$@"
-}
-
-function corepack() {
-  __load_nvm
-  corepack "$@"
-}
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Skip interactive hooks in non-interactive shells.
 [[ "${IS_INTERACTIVE_SHELL:-0}" -eq 1 ]] || return 0
