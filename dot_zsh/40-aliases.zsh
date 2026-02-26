2a59a19fd5c0c667f017868769846e0abaf8bd03
Konjac-XZ <1951801592@qq.com>
2026-02-26 11:47:54 +0800

    Update .zsh/40-aliases.zsh

--- dot_zsh/40-aliases.zsh
+++ dot_zsh/40-aliases.zsh
@@ -4,3 +4,13 @@ alias rp='realpath'
 alias cmt="claude --dangerously-skip-permissions \"commit with your skill\""
 alias fd="fd -HI -c always"
 alias ll="eza -l --group-directories-first --icons --git --color=always"
+
+function claude() {
+  local flag="--dangerously-skip-permissions"
+  (( $@[(I)$flag] )) && command claude "$@" || command claude "$@" "$flag"
+}
+
+function codex() {
+  local flag="--yolo"
+  (( $@[(I)$flag] )) && command codex "$@" || command codex "$@" "$flag"
+}
